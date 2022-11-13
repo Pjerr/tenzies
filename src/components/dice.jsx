c8a597b0cc12af9fca7cbe00b63b40c5b9712bc0
Petar <petar.milisavljevic@elfak.rs>
2022-11-13 13:31:52 +0100

    added feature to freeze dice

--- src/components/dice.jsx
+++ src/components/dice.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 
 export class Dice extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleDie = this.toggleDie.bind(this);
+  }
+
+  toggleDie() {
+    this.props.toggleDie(this.props.diceObject.id);
+  }
   render() {
     return (
-      <div className="die-face">
-        <h2 className="die-value">{this.props.value}</h2>
+      <div
+        className={
+          this.props.diceObject.frozen ? "die-face frozen" : "die-face"
+        }
+      >
+        <h2 className="die-value" onClick={this.toggleDie}>
+          {this.props.diceObject.diceValue}
+        </h2>
       </div>
     );
   }
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -8,25 +8,55 @@ export class Main extends React.Component {
       diceArray: [],
     };
     this.state.diceArray = this.allNewDice();
+
+    this.rollNewDice = this.rollNewDice.bind(this);
+    this.freezeDice = this.freezeDice.bind(this);
   }
 
   allNewDice() {
     let array = [];
     for (let i = 0; i < 10; i++) {
       let value = Math.floor(Math.random() * 6 + 1);
-      array.push({ id: i, diceValue: value });
+      array.push({ id: i, diceValue: value, frozen: false });
     }
     return array;
   }
 
+  rollNewDice() {
+    const newDiceArray = this.allNewDice();
+    const oldDiceArray = this.state.diceArray;
+    console.log(newDiceArray);
+    oldDiceArray.map((diceObject) => {
+      if (diceObject.frozen) {
+        newDiceArray[diceObject.id] = { ...diceObject };
+      }
+    });
+    console.log(newDiceArray);
+    this.setState({ diceArray: newDiceArray });
+  }
+
+  freezeDice(index) {
+    const diceArray = this.state.diceArray.slice();
+    const oldDie = this.state.diceArray[index];
+    const newDie = { ...oldDie, frozen: !oldDie.frozen };
+    diceArray.splice(index, 1, newDie);
+    this.setState({ diceArray });
+  }
+
   render() {
     const diceData = this.state.diceArray.map((diceObject) => (
-      <Dice key={diceObject.id} value={diceObject.diceValue} />
+      <Dice
+        key={diceObject.id}
+        diceObject={diceObject}
+        toggleDie={this.freezeDice}
+      />
     ));
     return (
       <main className="main">
         <div className="dice-container">{diceData}</div>
-        <button></button>
+        <button className="roll-dice-button" onClick={this.rollNewDice}>
+          Roll
+        </button>
       </main>
     );
   }
